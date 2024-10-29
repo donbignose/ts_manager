522da47c121883fb9b3661c11bffa2e944822d63
donbignose <stamerra17@gmail.com>
2024-10-29 23:32:01 +0100

    refactor(templates): split templates into components and includes

--- ts_manager/settings.py
+++ ts_manager/settings.py
@@ -77,7 +77,7 @@ ROOT_URLCONF = "ts_manager.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
